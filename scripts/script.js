ad7e9cea78a36e284f33b676fc36480fac935b6f
Anton Kryukov <antohakriukow@gmail.com>
2020-09-10 22:07:27 +0300

    version 1.03 update 10.09.2020 22:07

--- scripts/script.js
+++ scripts/script.js
@@ -9,18 +9,15 @@ let popupAboutInput = popupSection.querySelector('.popup__input-field_about');
 let popupForm = popupSection.querySelector('.popup__input-container');
 let popupCloseBtn = popupSection.querySelector ('.popup__close-btn');
 
-console.log(userName.textContent);
-console.log(userAbout.textContent);
 
 function popupSwitch() {
   if (popupSection.classList.contains('popup_opened') === false) {
     popupNameInput.value = userName.textContent;
     popupAboutInput.value = userAbout.textContent;
-    popupSection.classList.toggle('popup_opened');
-  } else {
-    popupSection.classList.toggle('popup_opened');
   }
+  popupSection.classList.toggle('popup_opened');
 }
+
 
 function formSubmitHandler(evt) {
   evt.preventDefault();
@@ -30,6 +27,7 @@ function formSubmitHandler(evt) {
 
   popupSwitch();
 }
+
 
 userEditBtn.addEventListener('click', popupSwitch);
 popupCloseBtn.addEventListener('click', popupSwitch);
